1438a114c5d70b645608e5188b46e6a08deb4254
psh9321 <parksuhyun9321@gmail.com>
2026-05-29 23:31:14 +0900

    브라우저 타이틀 수정 및 좋아요 api 토큰 설정 수정
     - response 헤더에 토큰이 있을시 토큰 재설정

--- src/entities/favorite/list/api/api.server.favorite.list.ts
+++ src/entities/favorite/list/api/api.server.favorite.list.ts
@@ -1,10 +1,13 @@
 import { BACKEND_API } from "@/shared/api/server.instance";
+import { SetToken } from "@/shared/util/token";
 
 export async function API_SERVER_FAVORITE_LIST() {
     try {
         const api = await BACKEND_API.get("favorite")
         
         if(!api.ok) throw api.statusText;
+        
+        await SetToken(api);
 
         const result = await api.json() as API_FAVORITE_LIST;
 
